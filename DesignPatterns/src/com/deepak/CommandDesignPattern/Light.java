852a809a4d3f9450ddd5c20fba79705350b5d1e6
Deepak Wagadre <deepakwagadre@gmail.com>
2019-01-01 00:19:20 +0530

    Design Pattern

---
+++ DesignPatterns/src/com/deepak/CommandDesignPattern/Light.java
@@ -0,0 +1,14 @@
+package com.deepak.CommandDesignPattern;
+
+public class Light 
+{
+      public void turnLightsON()
+      {
+    	  System.out.println("Turning Lights ON .....");
+      }
+      
+      public void turnLightsOFF()
+      {
+    	  System.out.println(".... Turning Lights OFF");
+      }
+}
